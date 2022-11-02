9ef2851fdf0a706cd9d779abfe9db6810238975e
terrnit <terrnit.megaman@gmail.com>
2022-11-03 00:58:00 +0500

    docs(todo): todo: re-write auth flow

--- App.js
+++ App.js
@@ -4,20 +4,24 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import Main from "./src/screens/Main";
 import SignUp from "./src/screens/SingUp";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// TODO: Re-write auth flow
+//
+// return ternary
+// ? login, signUp, forget password screens
+// : main app
+//
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
